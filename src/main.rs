b43e15b54cbfc6958eca82e9115c5900aa1c080b
Randy Brown <thatrandybrown@gmail.com>
2025-11-05 21:07:36 -0600

    add the same two neighbors to every cell

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,11 @@ fn main() {
         };
         world.push(Rc::new(RefCell::new(cell)));
     }
+
+    for cell_rc in &world {
+        let mut cell = cell_rc.borrow_mut();
+        cell.neighbors = Some(vec![Rc::downgrade(&world[0]), Rc::downgrade(&world[1])]);
+    }
+
     println!("Hello, world!");
 }
