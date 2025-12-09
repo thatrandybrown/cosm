8b4a775510fbbe203919c7c6497b299f6c7f18fb
Randy Brown <thatrandybrown@gmail.com>
2025-12-08 22:07:46 -0600

    just the triangles

--- src/main.rs
+++ src/main.rs
@@ -58,13 +58,11 @@ fn main() {
 
         let row1 = nh_raw
             .clone()
-            .map(|s| if s { '▲' } else { ' ' })
-            .collect::<String>();
+            .map(|s| if s { '▲' } else { ' ' });
 
         let row3 = sh_raw
             .clone()
-            .map(|s| if s { '▼' } else { ' ' })
-            .collect::<String>();
+            .map(|s| if s { '▼' } else { ' ' });
 
         let nh = world.iter().take(5).map(|c| if c.borrow().state { '█' } else { ' ' }).collect::<String>();
         let sh = world.iter().skip(5).map(|c| if c.borrow().state { '█' } else { ' ' }).collect::<String>();
