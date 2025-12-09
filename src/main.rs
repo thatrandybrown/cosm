e3c802884f3471a80b16aa9a6d30c10e16471aa3
Randy Brown <thatrandybrown@gmail.com>
2025-12-08 21:33:32 -0600

    print some triangles

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,11 @@ fn main() {
         let nh_raw = world.iter().take(5).map(|c| c.borrow().state);
         let sh_raw = world.iter().skip(5).map(|c| c.borrow().state);
 
+        let row1 = nh_raw
+            .clone()
+            .map(|s| if s { '▲' } else { ' ' })
+            .collect::<String>();
+
         let nh = world.iter().take(5).map(|c| if c.borrow().state { '█' } else { ' ' }).collect::<String>();
         let sh = world.iter().skip(5).map(|c| if c.borrow().state { '█' } else { ' ' }).collect::<String>();
 
